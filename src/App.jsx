26b3ed82fece3ea94e9dc0b3ac2d5ddeb835dd85
friscocode99 <libertyvegas9@gmail.com>
2026-02-19 13:49:05 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Simple Counter</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
